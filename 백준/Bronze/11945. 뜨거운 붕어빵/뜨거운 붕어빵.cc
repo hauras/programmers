bb107501badd97d6a591af7b4a260f3edf32c59b
haura <92637707+hauras@users.noreply.github.com>
2025-01-17 09:51:30 +0900

    [Bronze IV] Title: 뜨거운 붕어빵, Time: 0 ms, Memory: 2024 KB -BaekjoonHub

--- 백준/Bronze/11945. 뜨거운 붕어빵/뜨거운 붕어빵.cc
+++ 백준/Bronze/11945. 뜨거운 붕어빵/뜨거운 붕어빵.cc
@@ -6,21 +6,20 @@ using namespace std;
 
 int main() {
     int N, M;
-    cin >> N >> M; // 붕어빵의 세로 크기와 가로 크기 입력받기
+    cin >> N >> M; 
 
-    vector<string> fish(N); // 붕어빵 모양 저장할 문자열 벡터
+    vector<string> fish(N); 
 
-    // 붕어빵 모양 입력받기
+   
     for (int i = 0; i < N; i++) {
-        cin >> fish[i]; // 한 줄씩 입력받기
+        cin >> fish[i]; 
     }
 
-    // 땅에 떨어졌을 때의 모양 출력 (좌우 반전)
     for (int i = 0; i < N; i++) {
-        for (int j = M - 1; j >= 0; j--) { // 각 행을 거꾸로 출력
+        for (int j = M - 1; j >= 0; j--) {
             cout << fish[i][j];
         }
-        cout << endl; // 한 줄 출력 후 개행
+        cout << endl; 
     }
 
     return 0;
