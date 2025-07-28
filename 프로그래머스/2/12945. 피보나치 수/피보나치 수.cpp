b79d11d98eb1d4c331c846d9aac114a1e173fdfb
haura <92637707+hauras@users.noreply.github.com>
2025-07-28 16:13:43 +0900

    [level 2] Title: 피보나치 수, Time: 0.32 ms, Memory: 4.14 MB -BaekjoonHub

---
+++ 프로그래머스/2/12945. 피보나치 수/피보나치 수.cpp
@@ -0,0 +1,22 @@
+#include <string>
+#include <vector>
+
+using namespace std;
+
+int solution(int n) {
+    int answer = 0;
+    
+    int num1 = 0; // f(0)
+    int num2 = 1; // f(1)
+    
+    for(int i = 2; i<=n; i++)
+    {
+        answer = (num1 + num2) %1234567;
+        num1 = num2;
+        num2 = answer;
+    }
+    return answer;
+    
+    
+    return answer;
+}
