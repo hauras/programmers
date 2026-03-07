ff7e9bf1e5a35ba3c04fbddffa1a5f964991e560
haura <92637707+hauras@users.noreply.github.com>
2026-03-07 11:37:18 +0900

    [level 2] Title: 스킬트리, Time: 0.01 ms, Memory: 4.21 MB -BaekjoonHub

---
+++ 프로그래머스/2/49993. 스킬트리/스킬트리.cpp
@@ -0,0 +1,27 @@
+#include <string>
+#include <vector>
+
+using namespace std;
+
+int solution(string skill, vector<string> skill_trees) {
+    int answer = 0;
+    
+    for(string s : skill_trees)
+    {
+        string temp = "";
+        
+        for(char c : s)
+        {
+            if(skill.find(c) != string::npos)
+            {
+                temp += c;
+            }
+        }
+        
+        if(skill.find(temp) == 0)
+        {
+            answer++;
+        }
+    }
+    return answer;
+}
