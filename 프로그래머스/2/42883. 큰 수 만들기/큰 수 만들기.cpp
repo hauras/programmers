d3978be1a054653e9e6c9d6968199b83ec603748
haura <92637707+hauras@users.noreply.github.com>
2026-03-08 10:46:45 +0900

    [level 2] Title: 큰 수 만들기, Time: 4.70 ms, Memory: 5.56 MB -BaekjoonHub

---
+++ 프로그래머스/2/42883. 큰 수 만들기/큰 수 만들기.cpp
@@ -0,0 +1,27 @@
+#include <string>
+#include <vector>
+
+using namespace std;
+
+string solution(string number, int k) {
+    string answer = "";
+    
+    for(int i = 0; i<number.length(); i++)
+    {
+        
+        while(answer.length() > 0 && k > 0 && answer.back() < number[i])
+        {
+            answer.pop_back(); 
+            k--;
+        }
+        answer.push_back(number[i]);
+
+      
+    }
+    
+    if (k > 0) 
+    {
+        answer = answer.substr(0, answer.length() - k);
+    }
+    return answer;
+}
